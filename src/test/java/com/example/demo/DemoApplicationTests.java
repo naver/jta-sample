2e77f76f0c5f7df1fae30d1150a4434e0f92a0a5
daemin.hwang <daemin.hwang@navercorp.com>
2018-08-27 14:06:35 +0900

    디버깅 레벨 추가

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
 import com.example.demo.service.FooService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
@@ -17,6 +18,7 @@ public class DemoApplicationTests {
 
     @Test
     public void contextLoads() {
+        log.debug("test: {}", "=========================================================");
         fooService.bar("test");
     }
 
